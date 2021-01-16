21de0aac2ad44e9c2c6af300be374cc2064ba304
Xarsis <pi@raindrop.cloud9>
2021-01-16 04:33:53 -0500

    Updated example files (CSS and index) to implement click-based drop-downs for the months

--- examples/index.php
+++ examples/index.php
@@ -6,6 +6,16 @@
   <title>Comics Index</title>
   <link rel="stylesheet" href="comics.css" media="keep-going" onload="this.media='all'">
   <noscript><link rel="stylesheet" href="comics.css"></noscript>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $(".clickBtn").click(function() {
+        var row = $(this).data("row");
+        $('div[data-row='+row+']').toggle();
+        $('.clickBtn[data-row='+row+']').toggleClass('clicked');
+      });
+    });
+  </script>
 </head>
 
 <body>
@@ -14,30 +24,36 @@
 <table>
   <tr>
 <?php
-  $directory = '/share/Web/comics/html'; //Set this to your comic html folder
+  $directory = '/share/Web/comics/html';
   $scanned_dir = array_diff(scandir($directory), array('..', '.'));
-  $first_page = 0;
+  $first_page = 0; // has the first page been seen?  lets the script know if the comic starts in the middle of the month
   $column = 0;
-  $columnMax = 5; //Sets the table width (one month per column)
+  $columnMax = 5;
+  $row = 0;
 
   foreach ($scanned_dir as $key => $value) {
     if(substr($value, 0, 7) == 'comics-') {
       $date = substr($value, 7, 8);
       if(!($first_page) || (substr($date, 6, 2) == '01')) {
-        $column += 1;
         if($first_page)
-          print("    </td>\n");
+          print("    </div></td>\n");
         else
-          $first_page = 1;
-        if($column % ($columnMax + 1) == 0) {
+          $first_page = 1; // first page has been seen now
+        if($column % ($columnMax) == 0) { // start new row
+          $row += 1;
           print("  </tr>\n  <tr>\n");
+          //print("    <td>\n      <h2><a href=\"javascript:void()\" class=\"clickBtn\" data-row=\"$row\"></a></h2>\n    </td>\n");
+          //print("    <td>\n      <button class=\"clickBtn\" data-row=\"$row\"></button>\n    </td>\n");
         }
         // Arbitrary use of 3rd day of month to avoid time zone issue
         $month = date("F", mktime(0, 0, 0, substr($date, 4, 2), 3, 2020));
         $year = substr($date, 0, 4);
-        print("    <td>\n      <h2>$month $year</h2>\n");
+        print("    <td>\n      <button class=\"clickBtn\" data-row=\"$row\">$month $year</button>\n");
+        //print("    <td>\n      <h2><a href=\"javascript:void()\" class=\"clickBtn\" data-row=\"$row\">$month $year</a></h2>\n");
+        print("      <div class=\"list\" data-row=\"$row\" data-col=\"$column\">\n");
+        $column += 1;
       }
-      print("      <a href=\"$value\">$value</a><br>\n");
+      print("        <a href=\"$value\">$value</a><br>\n");
     }
   }
 ?>
